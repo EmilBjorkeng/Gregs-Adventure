808cb75efd9c0f2b832a1bda4f9cee428b85ab39
EmilBjorkeng <emil.bjorkeng@hotmail.com>
2022-05-18 14:54:57 +0200

    Added Crouching

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ clock = pygame.time.Clock()
 from settings import *
 boxes.append(Box(display, 0, 580, 800, 10))
 boxes.append(Box(display, 500, 550, 10, 30))
+boxes.append(Box(display, 100, 470, 50, 10))
+boxes.append(Box(display, 150, 450, 50, 10))
 
 # Player
 from player import *
@@ -26,11 +28,11 @@ while running:
     keys = pygame.key.get_pressed()
     # Left
     if keys[pygame.K_a]:
-        greg.move(-greg.speed)
+        greg.move(-1)
         greg.facing_left = True
     # Right
     if keys[pygame.K_d]:
-        greg.move(greg.speed)
+        greg.move(1)
         greg.facing_left = False
     # Jump
     if keys[pygame.K_SPACE]:
@@ -39,6 +41,10 @@ while running:
             held_space += 1
     elif held_space > 0:
         held_space -= 0.5
+    # Crouch
+    greg.is_crouching = False
+    if keys[pygame.K_LSHIFT]:
+        greg.is_crouching = True
 
     greg.update()
 
@@ -50,15 +56,17 @@ while running:
     for i in boxes:
         i.draw()
 
+    move_down_by = 0
+    if greg.is_crouching:
+        move_down_by = greg.crouch_decrees
+
     # OnGround Hitbox
-    #pygame.draw.rect(display, (0, 255, 0), (
-    #    greg.x + 5,
-    #    greg.y + greg.size - math.floor(greg.size / 5) + 1,
-    #    greg.size - 10,
-    #    math.floor(greg.size / 5) + 1))
+    #pygame.draw.rect(display, (0, 255, 0), (greg.x + 5, greg.y + greg.size - math.floor(greg.size / 5) + 1, greg.size - 10, math.floor(greg.size / 5) + 1))
+    # Head
+    #pygame.draw.rect(display, (255, 0, 0), (greg.x + greg.padding, greg.y + move_down_by, greg.size - greg.padding * 2, 15))
     # Hitting Wall Hitbox
-    #pygame.draw.rect(display, (255, 255, 0), (greg.x + greg.padding, greg.y, 15, greg.size - 10))
-    #pygame.draw.rect(display, (255, 255, 0), (greg.x + greg.size - 15 - greg.padding, greg.y, 15, greg.size - 10))
+    #pygame.draw.rect(display, (255, 255, 0), (greg.x + greg.padding, greg.y + 15 + move_down_by, 15, greg.size - 24 - move_down_by))
+    #pygame.draw.rect(display, (255, 255, 0), (greg.x + greg.size - 15 - greg.padding, greg.y + 15 + move_down_by, 15, greg.size - 24 - move_down_by))
 
     pygame.display.update()
     clock.tick(fps)
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from turtle import speed
 import pygame
 import math
 from settings import *
@@ -10,16 +11,21 @@ class Player:
         self.vx = 0
         self.vy = 0
         self.speed = 2.7
+        self.crouch_speed_mult = 0.7
         self.jump_force = 20
+        self.jump_force_crouch = 7
         self.jump_cooldown = 0
         self.mass_while_jumping = 0.001
         self.terminal_vel = 10
         self.mass = 0.8
         self.size = 50
         self.padding = 5
-        self.sprite = pygame.image.load(r'./Greg.png')
+        self.crouch_decrees = 16
+        self.sprite = pygame.image.load(r'./Assets/Greg.png')
         self.sprite = pygame.transform.scale(self.sprite, (self.size, self.size))
-        self.walk_animation_sprite = pygame.image.load(r'./gregwalk.png')
+        self.crouch_sprite = pygame.image.load(r'./Assets/gregcrouch.png')
+        self.crouch_sprite = pygame.transform.scale(self.crouch_sprite, (self.size, self.size))
+        self.walk_animation_sprite = pygame.image.load(r'./Assets/gregwalk.png')
         self.walk_animation_sprite = pygame.transform.scale(self.walk_animation_sprite, (self.size * 5, self.size))
         self.walk_animation = [
             self.walk_animation_sprite.subsurface(0, 0, self.size, self.size),
@@ -28,35 +34,68 @@ class Player:
             self.walk_animation_sprite.subsurface(self.size * 3, 0, self.size, self.size),
             self.walk_animation_sprite.subsurface(self.size * 4, 0, self.size, self.size)
         ]
+        self.crouch_walk_animation_sprite = pygame.image.load(r'./Assets/gregcrouchwalk.png')
+        self.crouch_walk_animation_sprite = pygame.transform.scale(self.crouch_walk_animation_sprite, (self.size * 5, self.size))
+        self.crouch_walk_animation = [
+            self.crouch_walk_animation_sprite.subsurface(0, 0, self.size, self.size),
+            self.crouch_walk_animation_sprite.subsurface(self.size, 0, self.size, self.size),
+            self.crouch_walk_animation_sprite.subsurface(self.size * 2, 0, self.size, self.size),
+            self.crouch_walk_animation_sprite.subsurface(self.size * 3, 0, self.size, self.size),
+            self.crouch_walk_animation_sprite.subsurface(self.size * 4, 0, self.size, self.size)
+        ]
         self.animation_frame = 0
         self.facing_left = False
         self.walking = False
         self.onGround = False
         self.is_jumping = False
+        self.is_crouching = False
 
     def move(self, value):
-        self.vx += value
+        speed_mult = 1
+        if self.is_crouching and self.onGround:
+            speed_mult = self.crouch_speed_mult
+        self.vx += value * self.speed * speed_mult
         self.walking = True
 
     def jump(self, held_space):
         if self.onGround:
             if self.jump_cooldown <= 0 or held_space < 5:
-                self.vy = -self.jump_force
+                if self.is_crouching:
+                    self.vy = -self.jump_force_crouch
+                else:
+                    self.vy = -self.jump_force
                 self.is_jumping = True
                 self.jump_cooldown = 5
             else:
                 self.jump_cooldown -= 1
     
     def draw(self):
-        if self.walking:
-            image = self.walk_animation[math.floor(self.animation_frame)]
-            self.animation_frame += 0.1
-            if self.animation_frame >= len(self.walk_animation):
-                self.animation_frame = 0
+        # Crouch
+        if self.is_crouching:
+            # Crouch Walking
+            if self.walking:
+                image = self.crouch_walk_animation[math.floor(self.animation_frame)]
+                self.animation_frame += 0.1
+                if self.animation_frame >= len(self.walk_animation):
+                    self.animation_frame = 0
+            # Crouching
+            else:
+                image = self.crouch_sprite
+        # Not Crouch
         else:
-            image = self.sprite
+            # Walking
+            if self.walking:
+                image = self.walk_animation[math.floor(self.animation_frame)]
+                self.animation_frame += 0.1
+                if self.animation_frame >= len(self.walk_animation):
+                    self.animation_frame = 0
+            # Standing
+            else:
+                image = self.sprite
+        # Facing Left
         if self.facing_left:
             image = pygame.transform.flip(image, True, False)
+        # Draw
         self.dis.blit(image, (self.x, self.y))
         self.walking = False
 
@@ -95,11 +134,15 @@ class Player:
                 self.vy = 0
 
     def hit_box_calculation(self):
+        move_down_by = 0
+        if self.is_crouching:
+            move_down_by = self.crouch_decrees
+
         for b in boxes:
             # Hitting wall Hitbox
-            for h in range(0, self.size - 10, 1):
+            for h in range(0, self.size - 24 - move_down_by, 1):
                 for w in range(0, 15, 1):
-                    if self.y + h > b.y and self.y + h < b.y + b.sizeY:
+                    if self.y + move_down_by + 15 + h > b.y and self.y + move_down_by + 15 + h < b.y + b.sizeY:
                         if self.x + self.padding + w > b.x and self.x + self.padding + w < b.x + b.sizeX:
                             self.x = b.x + b.sizeX - self.padding
                         elif self.x + self.size - 15 - self.padding + w > b.x and self.x + self.size - 15 - self.padding + w < b.x + b.sizeX:
@@ -112,6 +155,13 @@ class Player:
                         if self.x + 5 + w > b.x and self.x + 5 + w < b.x + b.sizeX:
                             self.y = b.y - self.size
                             self.onGround = True
+            
+            # Head
+            for h in range(0, 15, 1):
+                for w in range(0, self.size - self.padding * 2, 1):
+                    if self.y + move_down_by + h > b.y and self.y + move_down_by + h < b.y + b.sizeY:
+                        if self.x + self.padding + w > b.x and self.x + self.padding + w < b.x + b.sizeX:
+                            self.y = b.y + b.sizeY - move_down_by
     
     def update(self):
         #print(str(self.vx) + " : " + str(self.vy))
